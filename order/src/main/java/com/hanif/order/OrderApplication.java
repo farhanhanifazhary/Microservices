8e4b3ae861baaed5e1cca0125f5badc4d11b9e74
Farhan Hanif Azhary <farhanhanifazhary@gmail.com>
2026-03-24 14:37:56 +0700

    Minggu ke-3

--- order/src/main/java/com/hanif/order/OrderApplication.java
+++ order/src/main/java/com/hanif/order/OrderApplication.java
@@ -2,11 +2,13 @@ package com.hanif.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
