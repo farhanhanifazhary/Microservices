2494711960d7a30c256e2753d666cf2a0f27ec15
Farhan Hanif Azhary <farhanhanifazhary@gmail.com>
2026-03-24 14:39:54 +0700

    Minggu ke-3

--- produk/src/main/java/com/hanif/produk/ProdukApplication.java
+++ produk/src/main/java/com/hanif/produk/ProdukApplication.java
@@ -2,12 +2,14 @@ package com.hanif.produk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ProdukApplication {
 
 	public static void main(String[] args) {
